47844e50bc161f35b4af12f1ce1360928f1771e3
minling <mzhao725@gmail.com>
2015-06-04 01:02:52 -0400

    solved grandma

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,14 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(question)
+  if question == question.upcase
+    "NO, NOT SINCE 1938!"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+  
+       
